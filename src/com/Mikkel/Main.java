4f2c4ed50161446d5581918b25301ff1e6ed6971
mikkelvase <Mikk864n@stud.kea.dkgit config --global user.email mikk864n@stud.eka.dk>
2020-05-14 10:11:50 +0200

    1 upload

---
+++ src/com/Mikkel/Main.java
@@ -0,0 +1,8 @@
+package com.Mikkel;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
---
+++ src/com/Mikkel/Test.java
@@ -0,0 +1,8 @@
+package com.Mikkel;
+
+public class Test {
+
+    public void test() {
+        System.out.println("hello world!");
+    }
+}
