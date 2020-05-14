ddc53d15ce1ecda0881cd318fbd0e0f2dc1338e9
mikkelvase <Mikk864n@stud.kea.dkgit config --global user.email mikk864n@stud.eka.dk>
2020-05-14 10:30:23 +0200

    2 upload hey

--- src/com/Mikkel/Test.java
+++ src/com/Mikkel/Test.java
@@ -5,5 +5,6 @@ public class Test {
     public void test() {
         System.out.println("hello world!");
         System.out.println("Hello world222222");
+        System.out.println("Hello new");
     }
 }
